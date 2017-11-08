ae8ca668f2b34f1ecc8e831476612e5b25edf56f
Samuel Campbell <robojeeves@hotmail.com>
2017-11-08 13:47:55 -0600

    Added code to control the progress values on the Student Advising page

--- app/src/main/java/com/example/roboj/a_charm/MainActivity.java
+++ app/src/main/java/com/example/roboj/a_charm/MainActivity.java
@@ -14,15 +14,21 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+import java.util.ResourceBundle;
 
 import com.github.lzyzsd.circleprogress.CircleProgress;
 import com.github.lzyzsd.circleprogress.DonutProgress;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
     private Button b_login;
-    private Button b_add10, b_add15, b_sub25;
+   // private Button b_add10, b_add15, b_sub25;
     private EditText tf_email, tf_password;
-    private DonutProgress pb_creditHrs;
+   // private DonutProgress pb_creditHrs;
+    private DonutProgress dp_completionStatus;
+    private TextView tv_completionStatus, tv_core, tv_degreeCore, tv_lowerDivision, tv_upperDivision, tv_supportCourses;
+    private ProgressBar pb_core, pb_degreeCore, pb_lowerDivision, pb_upperDivision, pb_supportCourses;
     private TextView tv_forgotPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
         tv_forgotPassword = (TextView) findViewById(R.id.tv_forgotPassword);
         tv_forgotPassword.setMovementMethod(LinkMovementMethod.getInstance());
 
-       //OpenStudentAdvisory();
+       OpenStudentAdvisory();
 
         b_login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,10 +64,56 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void OpenStudentAdvisory(){
+
         setContentView(R.layout.student_advisory);
+
+        // Create references to all progress bars
+        dp_completionStatus = (DonutProgress) findViewById(R.id.dpCompletionStatus );
+        pb_core = (ProgressBar) findViewById(R.id.pb_CoreCurriculum);
+        pb_degreeCore = (ProgressBar) findViewById(R.id.pb_DegreeCore);
+        pb_lowerDivision = (ProgressBar) findViewById(R.id.pb_LowerDivision);
+        pb_upperDivision = (ProgressBar) findViewById(R.id.pb_UpperDivision);
+        pb_supportCourses = (ProgressBar) findViewById(R.id.pb_SupportCourses);
+
+        // Create references to Text Views
+        tv_completionStatus = (TextView) findViewById(R.id.tv_CompletionStatus);
+        tv_core = (TextView) findViewById(R.id.tv_CoreCurriculum);
+        tv_degreeCore = (TextView) findViewById(R.id.tv_DegreeCore);
+        tv_lowerDivision = (TextView) findViewById(R.id.tv_LowerDivision);
+        tv_upperDivision = (TextView) findViewById(R.id.tv_UpperDivision);
+        tv_supportCourses = (TextView) findViewById(R.id.tv_SupportCourses);
+
+
+        //Set Values for each progress bar
+        String progress = Integer.toString(56); // Sets progress for completion status bar
+        int core = 100, degreeCore = 85, lowerDivision = 50, upperDivision = 35, supportCourses = 45;
+        dp_completionStatus.setDonut_progress(progress);
+        dp_completionStatus.setText(progress + "%");
+
+        //Assign values to the progress bars
+        pb_core.setProgress(core);
+        pb_degreeCore.setProgress(degreeCore);
+        pb_lowerDivision.setProgress(lowerDivision);
+        pb_upperDivision.setProgress(upperDivision);
+        pb_supportCourses.setProgress(supportCourses);
+
+        // set labels for each bar
+        tv_completionStatus.setText("Completion Status\n 67/120 Hrs");
+        tv_core.setText(R.string.core);
+        tv_core.append(" " + Integer.toString(core) + "%");
+        tv_degreeCore.setText(R.string.degreeCore);
+        tv_degreeCore.append(" " + Integer.toString(degreeCore) + "%");
+        tv_lowerDivision.setText(R.string.lowerDivision);
+        tv_lowerDivision.append(" " + Integer.toString(lowerDivision) + "%");
+        tv_upperDivision.setText(R.string.upperDivision);
+        tv_upperDivision.append(" " + Integer.toString(upperDivision) + "%");
+        tv_supportCourses.setText(R.string.supportCourses);
+        tv_supportCourses.append(" " + Integer.toString(supportCourses) + "%");
+
+
     }
 
-    private void OpenReportScreen() {
+   /* private void OpenReportScreen() {
         setContentView(R.layout.sa_report);
 
         b_add10 = (Button) findViewById(R.id.b_add10);
@@ -92,6 +144,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+    */
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
